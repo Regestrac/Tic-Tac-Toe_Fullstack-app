ad9173efc19015db8249d1a8644a6824952c3105
Sojo C Johny <sojocjohny37@gmail.com>
2022-08-31 16:07:50 +0530

    Added state & change event on signup & login

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 const Login = () => {
+    const [username,setUsername] = useState("")
+    const [password,setPassword] = useState("")
+
+    const login =()=>{
+
+    }
   return (
     <div className="login-form">
+        <h2>LOGIN</h2>
      <form>
        <div className="input-container">
          <label>Username </label>
-         <input type="text" name="uname" required />
+         <input type="text" name="uname" required
+         onChange={(e)=> setUsername(e.target.value)} />
        </div>
        <div className="input-container">
          <label>Password </label>
-         <input type="password" name="pass" required />
+         <input type="password" name="pass" required
+         onChange={(e)=> setPassword(e.target.value)} />
        </div>
        <div className="button-container">
-         <input type="submit" />
+         <input type="submit" onClick={login} />
        </div>
      </form>
    </div>
--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
 
 const Signup = () => {
+    const [user,setUser] = useState(null)
+    const [username,setUsername] = useState(null)
+    const [email,setEmail] = useState(null)
+    const [password,setPassword] = useState(null)
+
+    const signup =()=>{
+
+    }
   return (
     <div className="signup-form">
+        <h2>SIGNUP</h2>
      <form>
        <div className="input-container">
-         <label>Email </label>
-         <input type="email" name="uname" required />
+         <label>Full Name</label>
+         <input type="email" name="uname" required 
+         onChange={(e)=> setUser(...user, e.target.value)} />
        </div>
        <div className="input-container">
          <label>Username </label>
-         <input type="text" name="uname" required />
+         <input type="text" name="uname" required
+         onChange={(e)=> setUsername(...username, e.target.value)} />
+       </div>
+       <div className="input-container">
+         <label>Email </label>
+         <input type="email" name="uname" required
+         onChange={(e)=> setEmail(...email, e.target.value)} />
        </div>
        <div className="input-container">
          <label>Password </label>
-         <input type="password" name="pass" required />
+         <input type="password" name="pass" required
+         onChange={(e)=> setPassword(...password, e.target.value)} />
        </div>
        <div className="button-container">
-         <input type="submit" />
+         <input type="submit" onClick={signup} />
        </div>
      </form>
    </div>
