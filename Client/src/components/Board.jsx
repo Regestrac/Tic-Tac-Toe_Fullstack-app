67a4ca07d351a040f7d2523b0c9f8baea5c95ef3
Sojo C Johny <sojocjohny37@gmail.com>
2022-09-01 15:51:07 +0530

    Added Draw condition

--- Client/src/components/Board.jsx
+++ Client/src/components/Board.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 // import { useState } from 'react'
 import './Board.css'
 
-const Board = ({ boxes, onClick, winPlayer, setWinPlayer, setGameOver, resetBoard}) => {  //props coming from Game.jsx
+const Board = ({ boxes, onClick, winPlayer, setWinPlayer, setGameOver, resetBoard }) => {  //props coming from Game.jsx
   // const [newClass, setNewClass] = useState('win-alert');
-  const handleOk =()=>{
-      resetBoard()
-      setWinPlayer(null)
-      setGameOver(true)
+  const handleOk = () => {
+    resetBoard()
+    setWinPlayer(null)
   }
   return (
-      <div className='board'>
-        <div className='rows'>
-          {boxes.map((value, index) => {   // Iterating through the 'boxes' prop
-            const style = value === "X" ? "box X" : "box O";    //Giving a style depending on the value
-            return (
-              <div className={style} value={value} onClick={() => value === null && onClick(index)} key={index}>{value}</div>  //making a div with the value from the 'boxes'
-            )
-          })}
-        </div>
-        {winPlayer && 
-        <div className='win-alert' >
-          <h3>Player {winPlayer} won!</h3>
-          <button className='ok-button' onClick={handleOk}>OK</button>
-        </div>}
+    <div className='board'>
+      <div className='rows'>
+        {boxes.map((value, index) => {   // Iterating through the 'boxes' prop
+          const style = value === "X" ? "box X" : "box O";    //Giving a style depending on the value
+          return (
+            <div className={style} value={value} onClick={() => value === null && onClick(index)} key={index}>{value}</div>  //making a div with the value from the 'boxes'
+          )
+        })}
       </div>
+      {winPlayer &&                  //Popup to show if there is a winner or a draw
+        <div className='win-alert' >
+          {winPlayer === "Draw" ? <h3>Its a {winPlayer}</h3> : <h3>Player {winPlayer} won!</h3>}
+          <button className='ok-button' onClick={handleOk}>OK</button>
+        </div>
+      }
+    </div>
   )
 }
 
--- Client/src/components/Game.jsx
+++ Client/src/components/Game.jsx
@@ -6,52 +6,58 @@ import ResetBtn from './ResetBtn'
 
 const Game = () => {
   const winConditions = [
-    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]        //These are the possible win conditions
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]        //These are the possible win conditions
   ]
 
   const [boxes, setBoxes] = useState(Array(9).fill(null));   //setting box values as null at begining
   const [playerX, setPlayerX] = useState(true)               //setting playerX status as true at begining
-  const [scores, setScores] = useState({scoreX:0, scoreO:0})
+  const [scores, setScores] = useState({ scoreX: 0, scoreO: 0 })
   const [gameOver, setGameOver] = useState(false)
   const [winPlayer, setWinPlayer] = useState(null)
 
-  const handleBoxClick = (boxIndex) =>{                      //This gets the index of clicked box from Board.jsx
-    const updatedBox = boxes.map((value, index)=>{     //Iterating through each box to check the value and index
-      if(index === boxIndex){                          //Checking if the indexes of the box and the Click match
+  const handleBoxClick = (boxIndex) => {                      //This gets the index of clicked box from Board.jsx
+    const updatedBox = boxes.map((value, index) => {     //Iterating through each box to check the value and index
+      if (index === boxIndex) {                          //Checking if the indexes of the box and the Click match
         return playerX === true ? "X" : "O";           //Returning the value depending on the player
-      }else{
+      } else {
         return value;                                  //Returns the previous value of all other boxes
       }
     })
     const winner = checkWinner(updatedBox)      //checks for winner in the updated box values
-    if(winner){
-      if(winner === "O"){
-        let {scoreO} = scores;
+
+    if (winner) {
+      if (winner === "O") {
+        let { scoreO } = scores;
         scoreO += 1;                          //updates score if O won
-        setScores({...scores,scoreO});
+        setScores({ ...scores, scoreO });
         setWinPlayer("O")
-      }else{
-        let {scoreX} = scores;
+      } else if (winner === "X") {
+        let { scoreX } = scores;
         scoreX += 1;                         //updates score if X won
-        setScores({...scores,scoreX});
+        setScores({ ...scores, scoreX });
         setWinPlayer("X")
       }
     }
     setBoxes(updatedBox);        //updating box value on each click
     setPlayerX(!playerX);        //updating playerX status on each click
+
+    const checkDraw = (array) => {
+      array.every(e => e != null) && setWinPlayer("Draw")
+    }
+    checkDraw(updatedBox)
   }
 
-  const checkWinner = (boxes) =>{                   //checks for winner
-    for(let i=0; i < winConditions.length; i++){    //loops through the winconditions
-      const [x,y,z] = winConditions[i];
-      if(boxes[x] && boxes[x] === boxes[y] && boxes[y] === boxes[z]){     //checks if the condition satisfies with same value
+  const checkWinner = (boxes) => {                   //checks for winner
+    for (let i = 0; i < winConditions.length; i++) {    //loops through the winconditions
+      const [x, y, z] = winConditions[i];
+      if (boxes[x] && boxes[x] === boxes[y] && boxes[y] === boxes[z]) {     //checks if the condition satisfies with same value
         setGameOver(true)
         return boxes[x];
       }
     }
   }
 
-  const resetBoard = ()=>{            //Reset the board for new game
+  const resetBoard = () => {            //Reset the board for new game
     setGameOver(false)
     setBoxes(Array(9).fill(null))
     setPlayerX(true)
@@ -60,10 +66,10 @@ const Game = () => {
 
   return (
     <div className='game'>
-        <h2>TIC TAC TOE</h2>
-        <ScoreBoard scores={scores} playerX={playerX} />
-        <Board boxes={boxes} onClick={gameOver ? resetBoard : handleBoxClick} winPlayer={winPlayer} setWinPlayer={setWinPlayer} resetBoard={resetBoard} />   {/* Passing the data to Board.jsx */}
-        <ResetBtn resetBoard={resetBoard} />
+      <h2>TIC TAC TOE</h2>
+      <ScoreBoard scores={scores} playerX={playerX} />
+      <Board boxes={boxes} onClick={gameOver ? resetBoard : handleBoxClick} winPlayer={winPlayer} setWinPlayer={setWinPlayer} resetBoard={resetBoard} />   {/* Passing the data to Board.jsx */}
+      <ResetBtn resetBoard={resetBoard} />
     </div>
   )
 }
